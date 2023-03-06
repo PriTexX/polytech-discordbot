9884a3bfae7e1ec20fc22ce20a45deb5935ed88a
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-03-06 10:55:32 +0300

    removed on_member_join

--- bot/cogs/ExampleCog.py
+++ bot/cogs/ExampleCog.py
@@ -1,9 +1,4 @@
-from datetime import datetime
-import discord
-from discord import ui
 from discord.ext import commands
-
-from UI.discordModal import my_modal
 
 
 class ExampleCog(commands.Cog):
@@ -13,17 +8,6 @@ class ExampleCog(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print("ready")
-
-    bot = commands.Bot(command_prefix="!")
-
-    @bot.event # inside the function describes the creation of view calling my_model after clicking on the button,
-    # the button can be removed :)
-    async def on_member_join(member):
-        view = ui.View()
-        modal = my_modal()
-        view.add_item(ui.Button(label="Click me", custom_id="my_button", row=0))
-        await modal.start(member=member, view=view)
-
 
     @commands.command()
     async def test(self, ctx):
