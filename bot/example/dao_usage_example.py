339374e8d5de9331d9397b6fa219991b69cb0e0b
pritexx <vladon_2004@mail.ru>
2023-03-17 13:15:20 +0300

    added update user method and saveOrUpdate or UserDAO

--- bot/example/dao_usage_example.py
+++ bot/example/dao_usage_example.py
@@ -9,22 +9,22 @@ from config.config import DSN
 async def run():
     async with asyncpg.create_pool(dsn=DSN, command_timeout=60) as pool:
         user_dao = UserDAO(pool)
-        users = await user_dao.get_all_users()
+        users = await user_dao.getAllUsers()
 
         print("Получаем всех пользаков")
         for user in users:
             print(user)
 
-        user = await user_dao.get_user(1)
+        user = await user_dao.getUser(1)
         print("Получаем пользователя который есть " + str(user))
 
-        user = await user_dao.get_user(4)
+        user = await user_dao.getUser(4)
         print("Получаем пользователя которого нет " + str(user))
 
-        rows = await user_dao.save_user(UserEntity(1, 'ab80cb36-6c80-4ba8-84fb-823eb7da28a5'))
+        rows = await user_dao.saveUser(UserEntity(1, 'ab80cb36-6c80-4ba8-84fb-823eb7da28a5'))
         print("Insert User пользователь существут поэтому ждем False -> " + str(rows))
 
-        rows = await user_dao.save_user(UserEntity(random.randint(1000, 100000), 'ab80cb36-6c80-4ba8-84fb-823eb7da28a5'))
+        rows = await user_dao.saveUser(UserEntity(random.randint(1000, 100000), 'ab80cb36-6c80-4ba8-84fb-823eb7da28a5'))
         print("Insert User пользователь не существут поэтому ждем True -> " + str(rows))
 
 
--- bot/repository/dao/user_dao.py
+++ bot/repository/dao/user_dao.py
@@ -6,26 +6,40 @@ from repository.entity import UserEntity
 SELECT_ALL_USERS = 'SELECT discorduserid, onecguid FROM discordstudents'
 SELECT_USER = 'SELECT discorduserid, onecguid FROM discordstudents WHERE discorduserid = $1'
 INSERT_USER = 'INSERT INTO discordstudents(discorduserid, onecguid) VALUES ($1, $2)'
+UPDATE_USER = 'UPDATE discordstudents SET onecguid = $1 WHERE discorduserid = $2'
 
 
 class UserDAO(AbstractDAO):
 
-    async def get_all_users(self) -> List[UserEntity]:
+    async def getAllUsers(self) -> List[UserEntity]:
         result = await self._pool.fetch(SELECT_ALL_USERS)
         users = []
         for user_record in result:
             users.append(UserEntity(user_record['discorduserid'], user_record['onecguid']))
         return users
 
-    async def get_user(self, user_id: int) -> Union[UserEntity, None]:
+    async def getUser(self, user_id: int) -> Union[UserEntity, None]:
         result = await self._pool.fetchrow(SELECT_USER, user_id)
         if result is None:
             return None
         return UserEntity(result['discorduserid'], result['onecguid'])
 
-    async def save_user(self, user: UserEntity) -> bool:
-        remote_user = await self.get_user(user.id)
+    async def saveUser(self, user: UserEntity) -> bool:
+        remote_user = await self.getUser(user.id)
         if not remote_user:
             await self._pool.execute(INSERT_USER, user.id, user.external_id)
             return True
         return False
+
+    async def updateUser(self, user: UserEntity) -> UserEntity:
+        await self._pool.execute(UPDATE_USER, user.external_id, user.id)
+        return user
+
+    async def saveOrUpdateUser(self, user: UserEntity) -> UserEntity:
+        user_exists = not (await self.saveUser(user))
+
+        if user_exists:
+            await self.updateUser(user)
+
+        return user
+
