c88b29828605e99262486c9af719af5f4d6b571b
pritexx <vladon_2004@mail.ru>
2023-09-12 22:45:33 +0300

    fix: max_length of password in login modal was 25 when password is allowed to be greater

--- bot/ui/loginModalComponent.py
+++ bot/ui/loginModalComponent.py
@@ -8,8 +8,8 @@ class LoginModalComponent(ui.Modal, title="Окно авторизации"):
         self.interaction: discord.Interaction = None
         self.timeout = 60
 
-    loginInput = ui.TextInput(label="Введите логин: ", style=discord.TextStyle.short, placeholder='i.i.ivanov', required=True, max_length=25)
-    passwordInput = ui.TextInput(label="Введите пароль: ", placeholder="Stud123456!", required=True, max_length=20)
+    loginInput = ui.TextInput(label="Введите логин: ", style=discord.TextStyle.short, placeholder='i.i.ivanov', required=True, max_length=50)
+    passwordInput = ui.TextInput(label="Введите пароль: ", placeholder="Stud123456!", required=True, max_length=255)
 
     async def on_submit(self, interaction: discord.Interaction):
         self.interaction = interaction
